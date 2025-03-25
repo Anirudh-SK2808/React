f1ddc1f3b292114a6d536e16aba53ca4cfab234a
Anirudh-SK2808 <ramanu2808@gmail.com>
2025-03-25 18:50:35 +0530

    changed app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./admin";
 import CampaignReview from "./campaign";
 import Index from "./index1";
@@ -8,11 +7,26 @@ import Login from "./login";
 import Signup from "./signup";
 import Volunteer from "./volunteer";
 
-const App = () => {
-  return (
-  <Signup/>
-  );s
-};
 
-export default App;
+
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}>
+          <Route index element={<Index />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="campaign" element={<CampaignReview />} />
+          <Route path="login" element={<Login/>} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="volunteer" element={<Volunteer />} />
+         
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 
